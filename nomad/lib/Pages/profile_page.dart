b8789db00f74a59291933c799bebcddd775f0dd7
AbdallahAlshibl <a.y.alshibl@gmail.com>
2023-02-15 13:34:13 +0300

    change color

--- nomad/lib/Pages/profile_page.dart
+++ nomad/lib/Pages/profile_page.dart
@@ -13,7 +13,7 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.lightBlue,
     );
   }
 }
