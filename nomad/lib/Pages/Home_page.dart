44ce1bcf0aab93e8f0fc48c9c6d17a1cc12c94f7
YousifT <s201777430@kfupm.edu.sa>
2023-02-18 15:13:23 +0300

    Added ListTile

--- nomad/lib/Pages/Home_page.dart
+++ nomad/lib/Pages/Home_page.dart
@@ -20,66 +20,103 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          CarouselSlider(
-            items: [
-              //Image 1
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: AssetImage(imgLinks[0]),
-                    fit: BoxFit.cover,
+        body: Container(
+      color: Colors.brown,
+      height: double.maxFinite,
+      width: double.maxFinite,
+      child: Column(children: [
+        Row(
+          children: [
+            Expanded(
+              child: CarouselSlider(
+                items: [
+                  //Image 1
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage(imgLinks[0]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
+
+                  // Image 2
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage(imgLinks[1]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+
+                  // Image 3
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage(imgLinks[2]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+
+                //Slider Container properties
+                options: CarouselOptions(
+                  height: 180.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8,
                 ),
               ),
-
-              // Image 2
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: AssetImage(imgLinks[1]),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              // Image 3
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: AssetImage(imgLinks[2]),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ],
-
-            //Slider Container properties
-            options: CarouselOptions(
-              height: 180.0,
-              enlargeCenterPage: true,
-              autoPlay: true,
-              aspectRatio: 16 / 9,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enableInfiniteScroll: true,
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              viewportFraction: 0.8,
             ),
-          ),
-          Row(
-            children: [
-              Text("Hello", style: TextStyle(fontSize: 20)),
-            ],
-          )
-        ],
-      ),
-    );
+          ],
+        ),
+        Text(
+          "Hello",
+          style: TextStyle(color: Colors.red, fontSize: 40),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Card(
+                color: Colors.red,
+                child: ListTile(title: Text("Title")),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Card(
+                color: Colors.red,
+                child: ListTile(title: Text("Title")),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Card(
+                color: Colors.red,
+                child: ListTile(title: Text("Title")),
+              ),
+            ),
+          ],
+        )
+      ]),
+    ));
   }
 }
