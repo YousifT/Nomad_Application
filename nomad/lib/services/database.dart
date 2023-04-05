55335473acb29b65f58ad6532b1ea1e446a724dd
Shibl <90514429+AbdallahAlshibl@users.noreply.github.com>
2023-04-06 00:00:57 +0300

    added guide page and create database

---
+++ nomad/lib/services/database.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  // collection references:
+
+  // final CollectionReference userCollection =
+  //     FirebaseFirestore.instance.collection('users');
+}
