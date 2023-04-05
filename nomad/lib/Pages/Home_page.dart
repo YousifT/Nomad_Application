e472cf668c58bb469efecdd9c9a90e293ac37266
Khaled M <koko3490@hotmail.com>
2023-04-05 20:15:13 +0300

    category page

--- nomad/lib/Pages/Home_page.dart
+++ nomad/lib/Pages/Home_page.dart
@@ -1,7 +1,9 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:nomad/Pages/Category_page.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -20,83 +22,10 @@ List<String> imgLinks = [
 ];
 
 class _MyHomePageState extends State<MyHomePage> {
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      //maxFinite Height and Width to cover the whole screen
-      height: double.maxFinite,
-      width: double.maxFinite,
-      // Scrollable widget wrapping to hold the Column so it becomes scrollable
-      child: SingleChildScrollView(
-        child: Column(children: [
-          Row(
-            children: [
-              Expanded(
-                child: CarouselSlider(
-                  items: [
-                    //Image 1
-                    Container(
-                      margin: EdgeInsets.all(6.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage(imgLinks[0]),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-
-                    // Image 2
-                    Container(
-                      margin: EdgeInsets.all(6.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage(imgLinks[1]),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-
-                    // Image 3
-                    Container(
-                      margin: EdgeInsets.all(6.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage(imgLinks[2]),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ],
-
-                  //Slider Container properties
-                  options: CarouselOptions(
-                    height: 180.0,
-                    enlargeCenterPage: true,
-                    autoPlay: true,
-                    aspectRatio: 16 / 9,
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enableInfiniteScroll: true,
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    viewportFraction: 0.8,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Sublist("Events", "listItem1", "listItem2", "listItem3"),
-          Sublist("Restaurants", "listItem1", "listItem2", "listItem3"),
-          Sublist("Cafe", "listItem1", "listItem2", "listItem3")
-        ]),
-      ),
-    ));
-  }
-}
-
-Widget Sublist(
+    Widget Sublist(
     String title, String listItem1, String listItem2, String listItem3) {
   return Container(
     padding: const EdgeInsets.fromLTRB(8, 10, 4, 3),
@@ -112,7 +41,15 @@ Widget Sublist(
               title,
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            Icon(Icons.arrow_forward)
+             IconButton(
+            icon: Icon(Icons.add_box),
+            onPressed: () {
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => CategoryPage(Category:title,)),
+              );
+            },
+          ),
+            
           ],
         ),
         Expanded(
@@ -191,3 +128,80 @@ Widget Sublist(
     ),
   );
 }
+
+
+    return Scaffold(
+        body: Container(
+      //maxFinite Height and Width to cover the whole screen
+      height: double.maxFinite,
+      width: double.maxFinite,
+      // Scrollable widget wrapping to hold the Column so it becomes scrollable
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Row(
+            children: [
+              Expanded(
+                child: CarouselSlider(
+                  items: [
+                    //Image 1
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage(imgLinks[0]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+
+                    // Image 2
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage(imgLinks[1]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+
+                    // Image 3
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage(imgLinks[2]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+
+                  //Slider Container properties
+                  options: CarouselOptions(
+                    height: 180.0,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enableInfiniteScroll: true,
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    viewportFraction: 0.8,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Sublist("Events", "listItem1", "listItem2", "listItem3"),
+          Sublist("Restaurants", "listItem1", "listItem2", "listItem3"),
+          Sublist("Cafe", "listItem1", "listItem2", "listItem3")
+        ]),
+      ),
+    ));
+    
+  }
+}
+
