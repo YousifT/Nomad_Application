1768fa6e8ed8123b49a322498653471dd84158d9
YousifT <s201777430@kfupm.edu.sa>
2023-04-06 01:18:24 +0300

    assets file

--- nomad/macos/Flutter/GeneratedPluginRegistrant.swift
+++ nomad/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,7 @@ import firebase_auth
 import firebase_core
 import location
 
-
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
