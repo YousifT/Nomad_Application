b3ea73ddc664e6fb0ce73d19690306e683293370
Shibl <90514429+AbdallahAlshibl@users.noreply.github.com>
2023-04-06 16:29:17 +0300

    modified auth and database files

---
+++ nomad/lib/services/auth.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:nomad/main.dart';
+import 'package:nomad/services/database.dart';
+
+class Auth {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<User> getCurrentUser() async {
+    return await auth.currentUser!;
+  }
+
+  Future<void> createUserWithEmailAndPassword(String email, String fName,
+      String lName, String password, String username, DateTime createAt) async {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+
+    Map<String, dynamic> userInfoMap = {
+      'email': email,
+      'firstNamme': fName,
+      'lastName': lName,
+      'password': password,
+      'username': username,
+      'createAt': createAt,
+    };
+
+    if (userCredential != null) {
+      DatabaseService().addUserInfoToDB(auth.currentUser!.uid, userInfoMap);
+    }
+  }
+
+  Future<void> loginWithEmailAndPassword(String email, String password) async {
+    UserCredential userCredential =
+        await auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> logout() async {
+    await auth.signOut();
+  }
+}
--- nomad/lib/services/database.dart
+++ nomad/lib/services/database.dart
@@ -1,6 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  Future addUserInfoToDB(String userID, Map<String, dynamic> userInfoMap) {
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
+        .set(userInfoMap);
+  }
+
+  Future<DocumentSnapshot> getUserFromDB(String userID) async {
+    return FirebaseFirestore.instance.collection('users').doc(userID).get();
+  }
   // collection references:
 
   // final CollectionReference userCollection =
