74eb56221a92b40fd3fa9647c07cc4725e531089
Ahmed <Almulihiah@gmail.com>
2023-04-08 05:47:28 +0300

    profile and sittings

--- nomad/lib/Global_Var.dart
+++ nomad/lib/Global_Var.dart
@@ -8,6 +8,7 @@ import 'package:nomad/Pages/Home_page.dart';
 import 'package:nomad/Pages/Login%20page.dart';
 import 'package:nomad/Pages/Sginup%20page.dart';
 import 'package:nomad/Pages/UserProfile.dart';
+import 'package:nomad/Pages/SettingsMenu.dart';
 
 double? global_Latitude;
 double? global_Longitude;
@@ -23,7 +24,7 @@ var global_LoggedIn_Pages = [
 var global_GuestUser_Pages = [
   GuidePage(),
   MyHomePage(),
-  Mysginuppage(),
+  SettingsMenu(),
 ];
 
 var global_adminUser_Pages = [
