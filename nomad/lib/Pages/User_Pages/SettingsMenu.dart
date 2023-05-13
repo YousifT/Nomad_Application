89a1fa18b6d1b3f45f3e63a33b61e567cf81101a
Khaled M <koko3490@hotmail.com>
2023-05-13 22:16:59 +0300

    proposal page

--- nomad/lib/Pages/User_Pages/SettingsMenu.dart
+++ nomad/lib/Pages/User_Pages/SettingsMenu.dart
@@ -119,8 +119,8 @@ Widget buildButton(
   VoidCallback onPressed,
 ) {
   return SizedBox(
-    width: 250,
-    height: 50,
+    width: 300,
+    height: 70,
     child: ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
@@ -145,7 +145,7 @@ Widget buildButton(
           ),
           Container(
             width: 40,
-            height: 50, // Set the height to match the button
+            height: 70, // Set the height to match the button
             decoration: BoxDecoration(
               color: Color.fromARGB(255, 51, 97, 119),
               borderRadius: BorderRadius.only(
