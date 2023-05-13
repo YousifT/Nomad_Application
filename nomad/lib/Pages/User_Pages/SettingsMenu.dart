a5ae34b16aca62dbf63ceafc03beb58d8e891880
Khaled M <koko3490@hotmail.com>
2023-05-13 21:07:59 +0300

    settings page

--- nomad/lib/Pages/User_Pages/SettingsMenu.dart
+++ nomad/lib/Pages/User_Pages/SettingsMenu.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nomad/Global_Var.dart' as globals;
-import 'package:nomad/Pages/User_Pages/HelpCenterPage%20.dart';
+
 import 'package:nomad/Pages/Home_page.dart';
 import 'package:nomad/Pages/User_Pages/Proposal_Form.dart';
 import 'package:nomad/Pages/User_Pages/UserProfile.dart';
+import 'package:nomad/Pages/User_Pages/app_colors.dart';
 import 'package:nomad/main.dart';
 import 'package:nomad/Pages/Admin_Pages/Proposals_page.dart';
 import 'package:nomad/Pages/Admin_Pages/Reports_page.dart';
+
+import 'HelpCenterPage .dart';
+
 
 Future<void> signOut(BuildContext context) async {
   await FirebaseAuth.instance.signOut();
@@ -35,89 +39,132 @@ class SettingsMenu extends StatelessWidget {
           height: 60,
           padding: const EdgeInsets.all(30),
         ),
-        SizedBox(
-          height: 75,
-          width: 350,
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const UserProfile()),
-              );
-            },
-            child: Text("Profile"),
-          ),
+        buildButton(
+          context,
+          "Profile",
+          Icons.person,
+          () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const UserProfile()),
+            );
+          },
         ),
-        SizedBox(
-          height: 75,
-          width: 350,
-          child: ElevatedButton(
-            onPressed: () {
+        buildButton(
+          context,
+          "Submit a Proposal Form",
+          Icons.file_upload,
+          () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProposalForm(onSubmitted: () {})),
+            );
+          },
+        ),
+        buildButton(
+          context,
+          "Help Center",
+          Icons.help,
+          () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HelpCenterPage()),
+            );
+          },
+        ),
+        buildButton(
+          context,
+          "Logout",
+          Icons.logout,
+          () => signOut(context),
+        ),
+        Visibility(
+          visible: adminC,
+          child: buildButton(
+            context,
+            "Proposals",
+            Icons.description,
+            () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => ProposalForm(onSubmitted: () {})),
+                  builder: (context) => const ProposalPage(),
+                ),
               );
             },
-            child: Text("Submit a Proposal Form"),
           ),
         ),
-        SizedBox(
-          height: 75,
-          width: 350,
-          child: ElevatedButton(
-            onPressed: () {
+        Visibility(
+          visible: adminC,
+          child: buildButton(
+            context,
+            "Reports",
+            Icons.report,
+            () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => HelpCenterPage()),
+                MaterialPageRoute(builder: (context) => const ReportsPage()),
               );
             },
-            child: Text("Help Center"),
-          ),
-        ),
-        SizedBox(
-          height: 75,
-          width: 350,
-          child: ElevatedButton(
-            onPressed: () => signOut(context),
-            child: Text("Logout"),
-          ),
-        ),
-        Visibility(
-          visible: adminC,
-          child: SizedBox(
-            height: 75,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ProposalPage(),
-                  ),
-                );
-              },
-              child: Text("Proposals"),
-            ),
-          ),
-        ),
-        Visibility(
-          visible: adminC,
-          child: SizedBox(
-            height: 75,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ReportsPage()),
-                );
-              },
-              child: Text("Reports"),
-            ),
           ),
         ),
       ],
     );
   }
+
+Widget buildButton(
+  BuildContext context,
+  String text,
+  IconData icon,
+  VoidCallback onPressed,
+) {
+  return SizedBox(
+    width: 250,
+    height: 50,
+    child: ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: Color.fromARGB(255, 185, 157, 139),
+        onPrimary: AppColors.darkTextColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        padding: EdgeInsets.zero, // Remove the padding
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 40,
+            height: 50, // Set the height to match the button
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 51, 97, 119),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8),
+                bottomRight: Radius.circular(8),
+              ),
+            ),
+            child: Icon(
+              icon,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
+
+
+}
+
+
